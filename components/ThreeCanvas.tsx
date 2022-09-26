95eee51edf04095559ac9195f8035acb92225d6b
Teno Siswono <teno.siswono@gojek.com>
2022-09-26 11:42:20 +0700

    change gltf

--- components/ThreeCanvas.tsx
+++ components/ThreeCanvas.tsx
@@ -1,17 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-
 import ThreeModel from './ThreeModel'
-import { useTheme } from 'next-themes'
+import { ContactShadows, Environment } from '@react-three/drei'
 
 export default function ThreeCanvas() {
-  const { theme, resolvedTheme } = useTheme()
   return (
     <Canvas
       shadows
-      flat
-      linear
       style={{
         position: 'absolute',
         top: '112px',
@@ -22,8 +18,12 @@ export default function ThreeCanvas() {
       }}
       className="bg-skin-base pointer-events-none absolute -z-10"
     >
+      <ambientLight intensity={0.7} />
+      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
       <Suspense fallback={null}>
-        <ThreeModel dark={theme === 'dark' || resolvedTheme === 'dark'} />
+        <Environment preset="city" />
+        <ThreeModel />
+        <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
       </Suspense>
     </Canvas>
   )
